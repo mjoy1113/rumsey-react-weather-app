d479ab253863c8fd9ba8642b256df81d2d1e09cf
mjoy1113 <mjoy1113@gmail.com>
2022-12-08 18:31:32 -0500

    Copied code from CodeSandbox

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Rumsey React App!!!</p>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
-
-export default App;
